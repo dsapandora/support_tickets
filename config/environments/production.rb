16f5debcc23c1d66623fadcf8cfd83b15f90aa23
Ariel Vernaza <ariel.darkserver@gmail.com>
2017-01-08 18:21:31 +0000

    Config with sengrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,5 +75,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.stripe.publishable_key = 'pk_live_XXXYYYZZZ'
 end
