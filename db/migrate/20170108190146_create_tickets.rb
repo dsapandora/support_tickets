38e32f45ac91839dcacd60e27cd16594f35a249f
Ariel Vernaza <ariel.darkserver@gmail.com>
2017-01-08 19:07:03 +0000

    status reference in tickets

--- db/migrate/20170108190146_create_tickets.rb
+++ db/migrate/20170108190146_create_tickets.rb
@@ -2,7 +2,7 @@ class CreateTickets < ActiveRecord::Migration[5.0]
   def change
     create_table :tickets do |t|
       t.text :question
-      t.reference :status
+      t.references :status, foreign_key: true
       t.references :user, foreign_key: true
 
       t.timestamps
