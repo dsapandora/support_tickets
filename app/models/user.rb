2a019b72c7f5b69144a439d869baf27919d7f866
Ariel Vernaza <ariel.darkserver@gmail.com>
2017-01-08 18:51:40 +0000

    add migration to role

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,14 @@
 class User < ActiveRecord::Base
   # Include default devise modules.
   devise :database_authenticatable, :registerable,
-          :recoverable, :rememberable, :trackable, :validatable,
-          :confirmable, :omniauthable
+  :recoverable, :rememberable, :trackable, :validatable,
+  :confirmable, :omniauthable
   include DeviseTokenAuth::Concerns::User
   belongs_to :role
+  before_create :set_default_role
+
+  private
+  def set_default_role
+    self.role ||= Role.find_by_name('customer')
+  end
 end
